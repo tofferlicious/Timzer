d837d338d25e62ae9a4d4e81f594e57540a6e16e
toffer <cmoessle@gmail.com>
2017-05-07 15:14:11 +0200

    time table contract

--- app/src/main/java/de/nannenbach/timzer/db/WorkTimeContract.java
+++ app/src/main/java/de/nannenbach/timzer/db/WorkTimeContract.java
@@ -7,20 +7,22 @@ import android.provider.BaseColumns;
  */
 
 public class WorkTimeContract {
+    public static final String TABLE_NAME = "entry";
+    public static final String COLUMN_NAME_DATE = "date";
+    public static final String COLUMN_NAME_START = "start";
+    public static final String COLUMN_NAME_END = "end";
+    public static final String COLUMN_NAME_BREAK = "break";
+    public static final String COLUMN_NAME_INTERRUPTION = "interruption";
+    public static final String COLUMN_NAME_TRAVEL_TIME = "travel_time";
+    public static final String COLUMN_NAME_STATUS = "status";
+
     // To prevent someone from accidentally instantiating the contract class,
     // make the constructor private.
     private WorkTimeContract() {}
 
     /* Inner class that defines the table contents */
     public static class WorkTimeEntry implements BaseColumns {
-        public static final String TABLE_NAME = "entry";
-        public static final String COLUMN_NAME_DATE = "date";
-        public static final String COLUMN_NAME_START = "start";
-        public static final String COLUMN_NAME_END = "end";
-        public static final String COLUMN_NAME_BREAK = "break";
-        public static final String COLUMN_NAME_INTERRUPTION = "interruption";
-        public static final String COLUMN_NAME_TRAVEL_TIME = "travel_time";
-        public static final String COLUMN_NAME_STATUS = "status";
+
 
     }
 }
